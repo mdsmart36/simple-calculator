a79b79f39b48498e57f6b63745c385f8e4600232
Matt Smart <mdsmart36@gmail.com>
2015-10-02 18:02:22 -0500

    writing tests for inputs

--- UnitTestCalculator/UnitTestCalculator.cs
+++ UnitTestCalculator/UnitTestCalculator.cs
@@ -58,12 +58,118 @@ namespace UnitTestSimpleCalculator
         }
 
         [TestMethod]
-        public void TestValidateInput()
+        public void TestValidateInputAdd()
         {
             Calculator myCalc = new Calculator();
-            string input = "";
-
-            //Assert.AreEqual();
+            string input = "9 + 4";
+            Assert.AreEqual(Calculator.Action.ADD, myCalc.ValidateInput(input));
         }
+
+        [TestMethod]
+        public void TestValidateInputSubtract()
+        {
+            Calculator myCalc = new Calculator();
+            string input = "9 - 4";
+            Assert.AreEqual(Calculator.Action.SUBTRACT, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputMultiply()
+        {
+            Calculator myCalc = new Calculator();
+            string input = "9 * 4";
+            Assert.AreEqual(Calculator.Action.MULTIPLY, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputDivide()
+        {
+            Calculator myCalc = new Calculator();
+            string input = "9 / 4";
+            Assert.AreEqual(Calculator.Action.DIVIDE, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputModulus()
+        {
+            Calculator myCalc = new Calculator();
+            string input = "9 % 4";
+            Assert.AreEqual(Calculator.Action.MODULUS, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputExit()
+        {
+            Calculator myCalc = new Calculator();
+            string input = "   exit ";
+            Assert.AreEqual(Calculator.Action.EXIT, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputBye()
+        {
+            Calculator myCalc = new Calculator();
+            string input = "quit ";
+            Assert.AreEqual(Calculator.Action.EXIT, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputLASTQ()
+        {
+            Calculator myCalc = new Calculator();
+            string input = " lastq";
+            Assert.AreEqual(Calculator.Action.LASTQ, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputLAST()
+        {
+            Calculator myCalc = new Calculator();
+            string input = " last ";
+            Assert.AreEqual(Calculator.Action.LAST, myCalc.ValidateInput(input));
+        }
+        
+        [TestMethod]
+        public void TestValidateInputInvalidInput()
+        {
+            Calculator myCalc = new Calculator();
+            string input = " my name";
+            Assert.AreEqual(Calculator.Action.INVALID, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputConstantAssign()
+        {
+            Calculator myCalc = new Calculator();
+            string input = " j =   44";
+            Assert.AreEqual(Calculator.Action.CONSTANT_ASSIGN, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputConstantLookup()
+        {
+            Calculator myCalc = new Calculator();
+            myCalc.Constants.Add("d", 7);
+            string input = " d ";
+            Assert.AreEqual(Calculator.Action.CONSTANT_LOOKUP, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputConstantError()
+        {
+            Calculator myCalc = new Calculator();
+            myCalc.Constants.Add("d", 7);
+            string input = "d = 3";
+            Assert.AreEqual(Calculator.Action.CONSTANT_ERROR, myCalc.ValidateInput(input));
+        }
+
+        [TestMethod]
+        public void TestValidateInputConstantNotAssigned()
+        {
+            Calculator myCalc = new Calculator();
+            string input = "d";
+            Assert.AreEqual(Calculator.Action.CONSTANT_NOTASSIGNED, myCalc.ValidateInput(input));
+        }
+        
     }
 }
